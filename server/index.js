6ea49df770e0a8f6d2c80e8a1838f371578f131b
Kin Chan <xkinchanx@gmail.com>
2019-08-19 01:05:21 -0700

    [prog] server route to send data

--- server/index.js
+++ server/index.js
@@ -2,16 +2,17 @@ const express = require('express');
 const path = require('path');
 const parser = require('body-parser');
 
+const matches = require('./bin/mockData');
+
 const port = process.env.PORT || 4000;
 const app = express();
-
-app.use((req, res, next) => {
-  console.log(`req url: ${req.url} and method: ${req.method}`);
-  next();
-})
 
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/dist/')));
+
+app.get('/api/matches', (req, res) => {
+  res.status(200).send(matches);
+})
 
 app.listen(port, () => console.log('listening on port: ', port));
