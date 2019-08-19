ec32608c411c435916f2990b758d09b67857cf13
Kin Chan <xkinchanx@gmail.com>
2019-08-19 01:05:07 -0700

    [prog] mock data for server to proivde

---
+++ server/bin/mockData.js
@@ -0,0 +1,39 @@
+const matches = [
+  {
+    name: 'Emma',
+    bio: `Looking for a magical night
+    I'll ride that broomstick all night long`,
+    pictures: [
+      "https://i.redd.it/3v6gkbgnbos01.jpg",
+      "http://i.imgur.com/sU3wZG8.jpg",
+      "https://i.redd.it/t03bgyqngvn21.jpg"
+    ]
+  },
+  {
+    name: 'Ryan',
+    bio: `You, me, clothes off, rubber ducky, fall into another dimension, have to fight for our lives, kills the bad guy, become the king and queen of a whole new world.  Also we fuck`,
+    pictures: [
+      "https://i.redd.it/0pmauwxy1i4z.jpg",
+      "http://i.imgur.com/XQJRX.jpg",
+      "https://i.redd.it/0yjuay3ur1v11.jpg"
+    ]
+  },
+  {
+    name: 'Dwayne',
+    bio: `Blood, sweat, respect`,
+    pictures: [
+      "https://i.redd.it/rurvuu7o9ko21.jpg",
+    ]
+  },
+  {
+    name: 'Natalie',
+    bio: `Girl, boy, I don't care as long as I get to be rough`,
+    pictures: [
+      "https://i.redd.it/eiw0mpzn0fnz.png",
+      "https://i.imgur.com/lUogNVy.jpg",
+      "https://i.redd.it/b5imoip6nn3z.jpg"
+    ]
+  },
+]
+
+module.exports = matches;
