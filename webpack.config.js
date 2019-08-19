28321ee19c73097e9bded93d8f31459437fae2f7
Kin Chan <xkinchanx@gmail.com>
2019-08-18 23:58:13 -0700

    [ops] app setup with dependencies

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist/'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js[x]?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ["@babel/plugin-proposal-class-properties", "@babel/plugin-transform-runtime"]
+          }
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loaders: ['file-loader'], 
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+};
