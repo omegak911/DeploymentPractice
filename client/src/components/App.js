4ea33203698989be604aa735056783a7e54a3c3f
Kin Chan <xkinchanx@gmail.com>
2019-08-19 01:06:33 -0700

    [prog] nav between home and matches

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
 
+import Home from './Home';
+import Matches from './Match/Matches';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      view: 'home'
+    }
+  }
 
+  changeView = (view) => {
+    this.setState({ view });
+  }
+
+  renderView = () => {
+    let { view } = this.state;
+
+    if (view === 'home') {
+      return <Home changeView={this.changeView} />
+    } else if (view === 'matches') {
+      return <Matches />
     }
   }
 
   render() {
     return (
       <div>
-        Hello
+        {this.renderView()}
       </div>
     )
   }
